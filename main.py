a2011b2f312e0fee2225e76033f5fe2cfcc4de0f
ali momed <107433147+unmadsajsk@users.noreply.github.com>
2022-06-28 20:45:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -139,5 +139,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url=URL_HER+str(BOT_TOKEN))
+    bot.set_webhook(url='https://testbottelebot.herokuapp.com/'+str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
